6b94fc3c5be4c885179de3f570246ac7b8e7dfa5
Juan David <jdkastrillon@gmail.com>
2022-02-26 18:11:40 -0500

    Se redieña parcialmente el navbar

--- src/components/header/NavigateBar.tsx
+++ src/components/header/NavigateBar.tsx
@@ -1,64 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import MenuIcon from "@mui/icons-material/Menu";
 
 import IconsSesion from "./IconsSesion";
 import IconCidenet from "../layout/IconCidenet";
 import Badge from "@material-ui/core/Badge";
 import { CartContext } from "../../context/CartProvider";
-
+import { NavLink } from "react-router-dom";
 
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 
 import { Link } from "react-router-dom";
 
 const NavigateBar = () => {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+
   const { totalAmount } = React.useContext(CartContext);
 
   return (
-    <div className="flex flex-ai-c header__wrapper ">
-      <nav className="header__navbar flex flex-jc-sb flex-jc-ai-c">
-        <div className="header__wrapper-burgger-menu flex flex-jc-sb">
-          <div style={{ height: "10px" }}>
-            <IconsSesion></IconsSesion>
+    <div>
+      <nav className="navbar">
+        <div className="nav-container">
+          <NavLink to="/" className="nav-logo">
+            Cidenet
+          </NavLink>
+
+          <ul className={click ? "nav-menu" : "nav-menu active"} >
+            <li className="nav-item">
+              <NavLink to="/" className="nav-links active" onClick={handleClick}>
+                Inicio
+              </NavLink>
+            </li>
+            <li className="nav-item" onClick={handleClick}>
+              <NavLink to="/masculino" className="nav-links" onClick={handleClick}>
+               <button onClick={handleClick}>Masculino</button> 
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/femenino" className="nav-links" onClick={() => handleClick}>
+                Femenino
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/cart" className="nav-links" onClick={handleClick}>
+                Carrito
+              </NavLink>
+            </li>
+          </ul>
+          <div className="nav-icon" onClick={handleClick}>
+            <MenuIcon  />
           </div>
-        </div>
-
-        <div className="header__iconCidenet">
-            <button type="button">
-          <Link to="/">
-              <IconCidenet />
-          </Link>
-            </button>
-        </div>
-
-        <div className="flex flex-jc-sb header__wrapper-buttons">
-          <Link to="/">
-            <button className="header__buttons" type="button">
-              Inicio
-            </button>
-          </Link>
-          <Link to="/type/masculino">
-            <button className="header__buttons" type="button">
-              Masculino
-            </button>
-          </Link>
-          <Link to="/type/femenino">
-            <button className="header__buttons" type="button">
-              Femenino
-            </button>
-          </Link>
-          <Link to="/cart">
-            <div>
-              <Badge
-                badgeContent={totalAmount()}
-                color="primary"
-                style={{ color: "white" }}
-              >
-                <ShoppingCartOutlinedIcon
-                  style={{ color: "white" }}
-                ></ShoppingCartOutlinedIcon>
-              </Badge>
-            </div>
-          </Link>
         </div>
       </nav>
     </div>
